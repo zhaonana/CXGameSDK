00773a7aacede1209fa093bf9cde357cb0348362
NaNa <zhao_na_na@126.com>
2014-11-06 12:06:11 +0800

    CXParams修改

--- CXGameSDK/Model/CXPayParams.h
+++ CXGameSDK/Model/CXPayParams.h
@@ -10,4 +10,13 @@
 
 @interface CXPayParams : NSObject
 
+//商品ID
+@property (nonatomic, copy) NSString *good_id;
+//CP订单号
+@property (nonatomic, copy) NSString *cp_bill_no;
+//回调URL 如果不设置 请提供同一通知地址给我们 ，通知将统一发送到所提供的地址
+@property (nonatomic, copy) NSString *notify_url;
+//扩展参数 支付成功服务器通知接口将原样返回
+@property (nonatomic, copy) NSString *extra;
+
 @end
